9d7a760feae4505279d02089c23fcb71d9649533
Radhika45 <radhikaarora366@gmail.com>
2025-08-23 21:26:21 +0530

    code

--- main.py
+++ main.py
@@ -303,7 +303,7 @@ def main():
     # In order to use Session Tracking, create a Secret Key
     web_app.secret_key = "doctors-app-key-v1"
     # Run the App infinitely, till user wont quite
-    web_app.run()
+    web_app.run(port=5000)
     # web_app.run(port=5001) # optionally you can give the port number
 if __name__ == "__main__":
     main()
